8b364a7cbd4d39af8893a8df0ac87bd6eecfce07
Martin <martinnz.dev@gmail.com>
2026-03-16 19:33:37 +1300

    added Q.133

---
+++ medium/133 Clone Graph/ans.py
@@ -0,0 +1,25 @@
+# Definition for a Node.
+# class Node:
+#     def __init__(self, val=0, neighbors=None):
+#         self.val = val
+#         self.neighbors = neighbors if neighbors is not None else []
+
+class Solution:
+    def cloneGraph(self, node):
+        if not node:
+            return None
+        old_to_new = {}
+
+        def dfs(n):
+            if n in old_to_new:
+                return old_to_new[n]
+            copy = Node(n.val)
+            old_to_new[n] = copy
+            for nei in n.neighbors:
+                copy.neighbors.append(dfs(nei))
+            return copy
+
+        return dfs(node)
+
+
+# there's probably a cleaner way to do this
