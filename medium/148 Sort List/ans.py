be384e7d32f141abadc4f31fa339a805cd6f1ff8
Martin <martinnz.dev@gmail.com>
2026-07-15 22:20:28 +1200

    added Q.148

---
+++ medium/148 Sort List/ans.py
@@ -0,0 +1,26 @@
+class Solution:
+    def sortList(self, head):
+        if not head or not head.next:
+            return head
+
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        mid = slow.next
+
+        left = self.sortList(head)
+        right = self.sortList(mid)
+
+        dummy = ListNode()
+        tail = dummy
+        while left and right:
+            if left.val <= right.val:
+                tail.next, left = left, left.next
+            else:
+                tail.next, right = right, right.next
+            tail = tail.next
+        tail.next = left or right
+        return dummy.next
+
+# merge sort on a linked list, O(nlogn) time
